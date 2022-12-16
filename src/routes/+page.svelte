a49c45e2702d43d17022fe962a7563b42db55ba3
Nathan Gerryts <nathgerr@gmail.com>
2022-12-16 17:26:27 -0500

    add construction sign

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 </script>
 
 <div class="index">
+    <h1>This page is under construction!</h1>
 </div>
 
 <style> 
@@ -9,8 +10,9 @@
         text-align: center;
         display: block;
         margin: 20px auto;
-        height: 970px;
+        height: 370px;
         width: 1560px;
         z-index: 3;
+        background-color: black;
     }
 </style>
