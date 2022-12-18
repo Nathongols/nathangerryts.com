ad7f14980c0fef848b5f169fc844acbf41727a16
Nathan Gerryts <nathgerr@gmail.com>
2022-12-17 19:50:58 -0500

    Update: new scene effect

--- src/lib/scene.svelte
+++ src/lib/scene.svelte
@@ -5,7 +5,7 @@
 		PerspectiveCamera,
 		useFrame } from '@threlte/core'
 	import { 
-		SphereGeometry, 
+		DodecahedronGeometry, 
 		MeshBasicMaterial, 
 		Color, 
 		BoxGeometry} from 'three'
@@ -14,28 +14,34 @@
 	let rotationcamera = 0
 	useFrame(() => {
 		rotation += 0.001
-		rotationcamera -+ 0.01
+		rotationcamera -= 0.0005
 	})
-	
 </script>
 
 <PerspectiveCamera 
-	position={{ x: 0, y: 0 }}
+	position={{ x: 0, y: 0, z:4}}
 	rotation={{ x: 0, y:0}}
 	>
 </PerspectiveCamera>
 
 <Mesh rotation={{x:0, y:rotation}}
-	geometry={new BoxGeometry(1, 1, 1)}
+	geometry={new DodecahedronGeometry(0.5, 1)}
 	material={new MeshBasicMaterial({
-		color: new Color('#FF3E00').convertSRGBToLinear(),
+		color: new Color('#fa82a2').convertSRGBToLinear(),
 		wireframe: true
 	})}
 />
 <Mesh rotation={{x:rotation, y:-rotation}}
-	geometry={new BoxGeometry(1, 1, 1)}
+	geometry={new DodecahedronGeometry(1, 1)}
 	material={new MeshBasicMaterial({
-		color: new Color('#FF3E00').convertSRGBToLinear(),
+		color: new Color('#e0436d').convertSRGBToLinear(),
+		wireframe: true
+	})}
+/>
+<Mesh rotation={{x:rotationcamera, y:-rotationcamera}}
+	geometry={new DodecahedronGeometry(5, 1)}
+	material={new MeshBasicMaterial({
+		color: new Color('#404040').convertSRGBToLinear(),
 		wireframe: true
 	})}
 />
